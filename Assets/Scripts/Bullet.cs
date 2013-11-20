0d346e146258ebb826bbe3688b10647f706a4030
gdrtelles <rctelles@ucsc.edu>
2013-11-20 14:28:01 -0800

    simple prototype
    player can move, jump, and shoot also slightly animated
    enemies spawn and can be destroyed adding to your score

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour 
+{
+
+
+
+	void Start () 
+	{
+		// Destroy the bullet after 2 seconds if it doesn't get destroyed before then.
+		Destroy(gameObject, 1);
+	}
+
+
+
+	
+	void OnTriggerEnter2D (Collider2D col) 
+	{
+		// If it hits an enemy...
+		if(col.tag == "Enemy")
+		{
+			Debug.Log("hey");
+			// ... find the Enemy script and call the Hurt function.
+			col.gameObject.GetComponent<Enemy>().Hurt();
+
+
+
+			// Destroy the bullet.
+			Destroy (gameObject);
+		}
+
+
+}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+	public float spawnTime = 5f;	         // The amount of time between each spawn.
+	public float spawnDelay = 3f;	        // The amount of time before spawning starts.
+	public GameObject enemie;		       // Array of enemy prefabs.
+
+
+	void Start ()
+	{
+		// Start calling the Spawn function repeatedly after a delay .
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+
+	}
+
+
+	void Spawn ()
+	{
+
+		Instantiate(enemie, transform.position, transform.rotation);
+
+
+	}
+
+
+}
